716edf2cd2508845e07ac38e3b1cd3af16af0443
Vlad <vladalexandru.tarba@gmail.com>
2025-02-05 10:41:58 +0100

    Update (Kan nu continu gebruikt worden)

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,45 +1,43 @@
-﻿namespace ConsoleApp1
+﻿using System.ComponentModel.Design;
+
+namespace ConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            // vraag om kleur
-            Console.WriteLine("hoi, welke kleur wil je dat de console wordt? (Engelse namen kunnen ook worden gebruikt)");
-
-            // sla kleur op, dus zoiets: x = console.readline
-            string input = Console.ReadLine();
-
-            // als kleur is groen, maak console groen
-            if (input == "groen" || input == "green")
+            while (true)
             {
-                Console.WriteLine("De console is nu groen.");
-                Console.BackgroundColor = ConsoleColor.Green;
-                return;
-            }
+                // vraag om kleur
+                Console.WriteLine("hoi, welke kleur wil je dat de console wordt? (Engelse namen kunnen ook worden gebruikt)");
 
-            // als kleur is rood, maak console groen
-            if (input == "rood" || input == "red")
-            {
-                Console.WriteLine("Het wordt toch groen");
-                Console.BackgroundColor = ConsoleColor.Green;
-                return;
-            }
+                // sla kleur op, dus zoiets: x = console.readline
+                string input = Console.ReadLine()?.ToLower();
 
-            // als kleur is paars, maak console paars
-            if (input == "paars" || input == "purple" || input == "magenta")
-            {
-                Console.WriteLine("De console wordt nu paars");
-                Console.BackgroundColor = ConsoleColor.Magenta;
-                return;
-            }
-
-            // als het iets anders is, maak console paars
-            if (input != "rood" || input != "red" || input != "groen" || input != "green")
-            {
-                Console.WriteLine("Het wordt toch paars");
-                Console.BackgroundColor = ConsoleColor.Magenta;
-                return;
+                if (input == "stop")
+                {
+                    Console.WriteLine("het programma wordt nu gestopt");
+                    break;
+                }
+                
+                // alle kleuren die gebruikt worden.
+                if (input == "rood" || input == "red")
+                {
+                    Console.WriteLine("Het wordt nu groen");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                } else if (input == "groen" || input == "green")
+                {
+                    Console.WriteLine("Fuck jou het wordt rood");
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                }else if (input == "paars" || input == "purple" || input == "magenta")
+                {
+                    Console.WriteLine("De console wordt nu paars");
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                } else if (input != "rood" || input != "red" || input != "groen" || input != "green")
+                {
+                    Console.WriteLine("Het wordt toch paars");
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                }
             }
         }
     }
